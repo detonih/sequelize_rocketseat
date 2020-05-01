873717680e3292f79451440e2d48cd37f608470c
Henrique Detoni <detonihenrique@gmail.com>
2020-05-01 10:04:33 -0300

    push project to github

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'root',
+    password: '1234',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
